47b040d4592bc8b5de148a1d251f7d80c2fdce1a
amanpandey3956 <95525490+amanpandey3956@users.noreply.github.com>
2022-02-11 10:28:02 -0500

    Add files via upload

---
+++ basics program/gross salary.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main()
+
+{
+int a,b,c,d;
+printf("Enter basic salary of employee");
+scanf("%d",&a);
+b=a*20/100;
+c=a*40/100;
+d=a+b+c;
+printf("%d",d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
